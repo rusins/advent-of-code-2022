84c633d5a0df54a62a8044cd959a0acfac08c189
Raitis Krikis <raitis.krikis@gmail.com>
2022-12-03 18:13:59 +0200

    Add day 2

---
+++ day 02/day2.rs
@@ -0,0 +1,34 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+
+fn main() {
+    let file = File::open("input").unwrap();
+    let lines = BufReader::new(file).lines().map(|e| e.unwrap());
+
+    let mut score = 0;
+    for line in lines {
+        let parts: Vec<&str> = line.split(" ").collect();
+        let opponent = match parts[0] {
+            "A" => 1,
+            "B" => 2,
+            "C" => 3,
+            _ => panic!(),
+        };
+        let you = match parts[1] {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => panic!(),
+        };
+        score += you;
+        let adj = (opponent - you + 2) % 3;
+        match adj {
+            1 => score += 6,
+            2 => score += 3,
+            _ => (),
+        }
+    }
+
+    println!("{}", score)
+}
